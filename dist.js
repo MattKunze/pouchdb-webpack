426e862d1bf0d4f287efcbbf8b6e14a939045637
Matt Kunze <matt.kunze@datasplice.com>
2015-04-02 11:31:53 -0600

    initial files

---
+++ dist.js
@@ -0,0 +1,1 @@
+var PouchDb = require('pouchdb/dist/pouchdb');
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: {
+    normal: "./normal.js",
+    dist: "./dist.js"
+  },
+  output: {
+    path: __dirname,
+    filename: "[name]-bundle.js"
+  },
+  module: {
+    loaders: [{
+      test: /\.json$/,
+      loader: 'json'
+    }
+    ],
+    noParse: /(lie|pouchdb)\.js$/
+  }
+}
